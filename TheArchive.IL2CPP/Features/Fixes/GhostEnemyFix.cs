d2ea929d844bad85900226ea638c95ba1022b4a2
Nina <859994525zjy@gmail.com>
2024-11-16 11:50:49 +0800

    添加幽灵尸体修复

---
+++ TheArchive.IL2CPP/Features/Fixes/GhostEnemyFix.cs
@@ -0,0 +1,26 @@
+﻿using Enemies;
+using TheArchive.Core.Attributes;
+using TheArchive.Core.FeaturesAPI;
+
+namespace TheArchive.Features.Fixes
+{
+    [EnableFeatureByDefault]
+    public class GhostEnemyFix : Feature
+    {
+        public override string Name => "Ghost Enemy Fix";
+
+        public override string Description => "Avoid the appearance of ghost enemy bodies.";
+
+        public override FeatureGroup Group => FeatureGroups.Fixes;
+
+        [ArchivePatch(typeof(EnemyAgent), nameof(EnemyAgent.Alive), null, ArchivePatch.PatchMethodType.Setter)]
+        private class EnemyAgent__set_Alive__Patch
+        {
+            private static void Postfix(EnemyAgent __instance, bool value)
+            {
+                if (value)
+                    EnemyUpdateManager.Current.Register(__instance, __instance.CourseNode.m_enemyUpdateMode);
+            }
+        }
+    }
+}
